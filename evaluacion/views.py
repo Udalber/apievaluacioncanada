eccc1a1a3aa3367470eb70b45df73d7cab25047d
udalbert <udgottschalk@gmail.com>
2017-11-25 13:12:45 -0500

    Base Projecto

---
+++ evaluacion/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('base/base.html')
+    context = {}
+    return render(request,'base/base.html')
+
+def elegir(request):
+    template = loader.get_template('evaluacion/evaluacion.html')
+    context = {}
+    return render(request,'evaluacion/evaluacion.html')
